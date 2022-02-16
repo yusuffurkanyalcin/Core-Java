e3195ad9805c98faa2438e8ae31606233a2bd948
Yusuf Furkan Yalçın <55233993+yusuffurkanyalcin@users.noreply.github.com>
2022-02-16 23:22:21 +0300

    Update Customer.java

--- src/main/java/com/example/corejava/StaticFactory/Customer.java
+++ src/main/java/com/example/corejava/StaticFactory/Customer.java
@@ -21,6 +21,23 @@ public class Customer {
         this.country=country;
         this.category=category;
     }
+    
+    public static Customer createWithDefaultCategory(
+            String firstName,
+            String lastName,
+            String country
+    ){
+        return new Customer(firstName,lastName,country,"Bireysel");
+    }
+
+    public static Customer createWithoutDefault(
+            String firstName,
+            String lastName,
+            String country,
+            String category
+    ){
+                return new Customer(firstName,lastName,country,category);
+    }
 
 
 }
